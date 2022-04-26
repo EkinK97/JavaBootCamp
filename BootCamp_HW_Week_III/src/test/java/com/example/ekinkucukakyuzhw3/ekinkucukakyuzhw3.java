891d1373f7d20d6d4529a2a618b3ac4eac251683
Ekin Küçükakyüz <62599165+EkinK97@users.noreply.github.com>
2022-04-26 17:48:32 +0300

    Update ekinkucukakyuzhw3.java

--- BootCamp_HW_Week_III/src/test/java/com/example/ekinkucukakyuzhw3/ekinkucukakyuzhw3.java
+++ BootCamp_HW_Week_III/src/test/java/com/example/ekinkucukakyuzhw3/ekinkucukakyuzhw3.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DogukanAliGundoganH3ApplicationTests {
+class ekinkucukakyuzhw3ApplicationTests {
 
 	@Test
 	void contextLoads() {
